3e929daf261131cf68dba67259913d3c1da52c8a
abdel mouzahir <jalil.mouzahir@gmail.com>
2023-08-29 16:16:16 -0600

    build run

---
+++ build/precache-manifest.0e13860cf395b4dbf05d9c1b9045a7d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8e0d6bebdf6a36c08c3ed096fa0100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af7814e892b82378a858",
+    "url": "/static/js/2.17f1cb7c.chunk.js"
+  },
+  {
+    "revision": "700e6fc5b455dcc9a87a",
+    "url": "/static/js/main.f7e046cf.chunk.js"
+  },
+  {
+    "revision": "2f55955791534fe0d08b",
+    "url": "/static/js/runtime-main.1048f588.js"
+  }
+]);
